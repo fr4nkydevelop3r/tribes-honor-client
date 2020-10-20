ae6b3708fd4e34a9c41d4293f3ea3f6a92a69e54
Frank Andrade <fr4nky.develop3r@gmail.com>
2020-10-19 20:04:28 -0400

    feat: add styles signup

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -2,27 +2,35 @@ import React, { useState } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import axios from 'axios';
 import { isAuth } from './helpers';
+import { useForm } from 'react-hook-form';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
 import Layout from '../Layout';
 
 const Signup = () => {
-  const [values, setValues] = useState({
+  /*const [values, setValues] = useState({
     name: '',
     email: '',
     password: '',
-    buttonText: 'Submit',
+    buttonText: 'Signup',
   });
 
-  const { name, email, password, buttonText } = values;
+  const { name, email, password, buttonText } = values; */
 
-  const handleChange = (value) => (event) => {
+  const [successSignUp, setSuccessSignUp] = useState(false);
+
+  //form validation
+  const { register, handleSubmit, watch, errors } = useForm();
+
+  /*const handleChange = (value) => (event) => {
     setValues({ ...values, [value]: event.target.value });
-  };
+  };*/
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setValues({ ...values, buttonText: 'Submitting' });
+  const onSubmit = (data) => {
+    const { name, email, password } = data;
+    console.log(name, email, password);
+
+    //setValues({ ...values, buttonText: 'Submitting' });
     axios({
       method: 'POST',
       url: `${process.env.REACT_APP_API}/signup`,
@@ -30,48 +38,79 @@ const Signup = () => {
     })
       .then((response) => {
         console.log('EMAIL SENT SUCCESSFULLY WITH LINK AND TOKEN', response);
-        setValues({
-          ...values,
-          name: '',
-          email: '',
-          password: '',
-          buttonText: 'Submited',
-        });
         toast.success(response.data.message);
+        setSuccessSignUp(true);
       })
       .catch((error) => {
         console.log('SIGNUP ERROR', error.response.error);
-        setValues({ ...values, buttonText: 'Submit' });
+        //setValues({ ...values, buttonText: 'Signup' });
         toast.error(error.response.data.error);
       });
   };
 
   const signupForm = () => (
     <div className="signup-container">
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit(onSubmit)}>
         {<h1 className="title-form">Create account</h1>}
         <div className="first-row">
-          <label>FULL NAME</label>
-          <input onChange={handleChange('name')} type="text" value={name} />
-        </div>
-        <div className="row">
-          <label>Email</label>
-          <input onChange={handleChange('email')} type="email" value={email} />
-        </div>
-        <div className="row">
-          <label>PASSWORD</label>
+          <label className="field-name">FULL NAME</label>
           <input
-            onChange={handleChange('password')}
-            type="password"
-            value={password}
+            className="field-input"
+            // onChange={handleChange('name')}
+            type="text"
+            // value={name}
+            placeholder="Enter your name"
+            ref={register({ required: true })}
+            name="name"
           />
+          {errors.name && <span className="error">This field is required</span>}
         </div>
-        <div>
-          <button type="submit" onClick={handleSubmit}>
-            {buttonText}
+        <div className="row">
+          <label className="field-name">EMAIL</label>
+          <input
+            className="field-input"
+            // onChange={handleChange('email')}
+            type="email"
+            //value={email}
+            placeholder="Your email goes here"
+            name="email"
+            ref={register({
+              required: 'Enter your e-mail',
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                message: 'Enter a valid e-mail address',
+              },
+            })}
+          />
+          {errors.email && (
+            <span className="error">Please enter a valid email</span>
+          )}
+        </div>
+        <div className="row">
+          <label className="field-name">PASSWORD</label>
+          <input
+            className="field-input"
+            // onChange={handleChange('password')}
+            type="password"
+            // value={password}
+            placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
+            ref={register({ required: true, minLength: 6 })}
+            name="password"
+          />
+          {errors.password && (
+            <span className="error">
+              The password must be at least 6 characters
+            </span>
+          )}
+        </div>
+        <div className="btn-form">
+          <button className="btn" type="submit">
+            Sign Up
           </button>
         </div>
-        <Link to="/auth/password/forgot"> Forgot password</Link>
+        <p className="login">
+          Already have an account? <Link to="/signin"> Log in</Link>{' '}
+        </p>
       </form>
     </div>
   );
@@ -80,7 +119,13 @@ const Signup = () => {
     <Layout>
       <ToastContainer />
       {isAuth() ? <Redirect to="" /> : null}
-      {signupForm()}
+      {!successSignUp ? (
+        signupForm()
+      ) : (
+        <>
+          <div>Great! Check your inbox to activate your account</div>
+        </>
+      )}
     </Layout>
   );
 };
